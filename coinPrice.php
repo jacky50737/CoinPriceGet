75d2c3630b280f4560e38ae72218cbc347133e45
jacky50737 <jacky50737@gmail.com>
2021-04-19 16:22:06 +0800

    新增出版檔案 V0.1

---
+++ coinPrice.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by Jacky.
+ * Project Name: CoinPriceGet.
+ * Date Time: 2021/4/19 下午 04:20.
+ */
+
+declare(strict_types=1);
+
+if (isset($_GET["password"])) {
+    $password = $_GET["password"];
+} else {
+    $password = "XXXXXX";
+}
+
+if ($password == "善鼎") {
+
+    if (isset($_GET["market"])) {
+        $market = $_GET["market"];
+    } else {
+        $market = "LINK/USDT";
+    }
+
+    $url = 'https://api.binance.com';
+    $binanceApiKey = 'ysSQb0zgMyxsF3knmh44MS3vcqyvBpT80XKrpuk4xnRVf1a2fLiaQaER4JXSrnRt';
+    $binanceApiSecret = 'YaIYBzTXEJSh5nAAFLkYR1bGDMl4mwaNfvwe3dtULr5GvLodZTHTLCRei9aRs8kT';
+    $coinGet = '/api/v3/ticker/price?symbol=' . $market;
+    $url_all = $url . $coinGet;
+
+    $ch = curl_init();
+
+    try {
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $data = json_decode($response);
+        var_dump($data);
+        return $data['price'];
+    } catch (Exception $e) {
+        var_dump($e);
+        return "發生未知的錯誤：" . $e;
+    }
+} else {
+    var_dump("密碼錯誤");
+    return "密碼錯誤";
+}
+
+
