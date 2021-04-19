2b0b43a791eb1267ad5bfa666892cdf40a8678b3
jacky50737 <jacky50737@gmail.com>
2021-04-19 17:37:11 +0800

    V0.1 - test

--- coinPrice.php
+++ coinPrice.php
@@ -30,7 +30,7 @@ if ($password == "善鼎") {
 
     try {
         $response = json_decode(file_get_contents($url_all));
-        var_dump($response->price);
+        $response->price = (int)$response->price;
         $data = json_encode((array)$response->price);
     } catch (Exception $e) {
         $data = json_encode("發生未知的錯誤：" . $e);
